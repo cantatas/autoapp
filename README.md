abb0a249d3c0613df8bd8f0051bc0044098baa7e
cantatas <zennqann@foxmail.com>
2020-12-02 23:40:47 +0800

    add hot style

--- README.md
+++ README.md
@@ -27,3 +27,8 @@ yarn run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 字段说明
+data-beautify-name 编辑元素表单名称
+data-beautify-type 编辑元素表单类型
+data-beautify-refit 编辑元素文字修改
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="app-index-page">
-    <div class="login-page form">
+  <div 
+  data-beautify-type="0" 
+  class="app-index-page"
+  :style="themeData"
+  >
+    <div class="login-page form beautify-ui" data-beautify-type="1">
+      <!-- log -->
       <div class="row">
         <div class="logo box">logo</div>
       </div>
-      <div class="row">
-        <div class="row-body box">
+
+      <!-- 国家/地区 -->
+      <div
+        class="row beautify-ui"
+        data-beautify-type="2"
+        data-beautify-name="prefixer"
+      >
+        <div class="row-body box input-form">
           <div class="form-title">国家/地区</div>
           <div class="form-ele">
             <input type="text" placeholder="请选择" />
@@ -13,17 +24,33 @@
           <div class="form-options"></div>
         </div>
       </div>
-      <div class="row">
-        <div class="row-body box">
-          <div class="form-title" contenteditable="true" >账号</div>
+
+      <!-- 账号 -->
+      <div
+        class="row beautify-ui"
+        data-beautify-type="2"
+        data-beautify-name="name"
+      >
+        <div class="row-body input-form box">
+          <div data-beautify-refit="2name" class="form-title">账号</div>
           <div class="form-ele">
-            <input type="text" placeholder="请输入账号" />
+            <input
+              data-beautify-refit="2placeholder"
+              type="text"
+              placeholder="请输入账号"
+            />
           </div>
           <div class="form-options"></div>
         </div>
       </div>
-      <div class="row">
-        <div class="row-body box">
+
+      <!-- 密码 -->
+      <div
+        class="row beautify-ui"
+        data-beautify-type="3"
+        data-beautify-name="pwd"
+      >
+        <div class="row-body input-form box">
           <div class="form-title">密码</div>
           <div class="form-ele">
             <input type="password" placeholder="请输入密码" />
@@ -32,11 +59,13 @@
         </div>
       </div>
 
-      <div class="row box">
+      <!-- 登录 -->
+      <div data-beautify-type="5" class="row beautify-ui box">
         <a class="form-btn">登录</a>
       </div>
 
-      <div class="row box full-box">
+      <!-- 底部其他操作 -->
+      <div data-beautify-type="6" class="row box beautify-ui full-box">
         <a class="form-text-btn btn-align-left">忘记密码？</a>
         <a class="form-text-btn">进入汇率</a>
         <a class="form-text-btn btn-align-right">新人注册</a>
@@ -48,60 +77,50 @@
 <script>
 export default {
   name: "indexPage",
+  data() {
+    return {
+      themeData: {
+        '--font-color':'#333',
+        '--border-color':'#333',
+        '--bg-color':'#333',
+      },
+    };
+  },
 };
 </script>
 
 <style lang="less" scoped>
-.form {
-  font-size: 14px;
-  width: 80%;
-  margin: auto;
-  input {
-    appearance: none;
-    border: 0;
-    background: transparent;
-  }
-  .row {
-    margin-bottom: 8%;
-    .row-body {
-      border-bottom: 1px solid #999;
-      padding: 5px 0;
-      > * {
-        width: 100%;
-        flex: 1;
-      }
-      .form-title {
-        text-align: left;
-      }
+@import "@/assets/less/form.less";
+.app-index-page {
+  height: 100%;
+  .login-page {
+    .logo {
+      min-height: 50px;
     }
   }
   .form-btn {
-    border-radius: 5px;
-    display: block;
-    background: #ff7171;
-    width: 100%;
-    height: 44px;
-    line-height: 44px;
-    color: #fff;
-  }
-  .form-text-btn{
-    display: block;
-  }
-  .btn-align-left{
-    text-align: left;
-  }
-  .btn-align-right{
-    text-align: right;
-  }
-}
-.app-index-page {
-  .login-page {
-    .logo{
-      height: 100px;
-    }
-  }
-  .form-btn{
     margin-top: 30px;
+    background: var(--btn-bg-color);
+    border: 1px solid;
+    border-color: var(--btn-border-color);
+    border-radius: var(--btn-border-radius);
+  }
+  .input-form{
+      border-bottom: var(--input-border-thick) var(--input-border-style) var(--input-border-color);
+    &.bottom-border{
+      border-bottom: var(--input-border-thick) var(--input-border-style) var(--input-border-color);
+    }
+    &.full-border{
+      padding: 10px 15px;
+      border: var(--input-border-thick) var(--input-border-style) var(--input-border-color);
+      // 全显边框才支持半径样式
+      &.border-radius{
+        border-radius: var(--input-border-radius);
+      }
+    }
+    .form-title {
+      color: var(--font-color);
+    }
   }
 }
 </style>
